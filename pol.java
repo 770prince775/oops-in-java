a7077fde34f6614f521d51acc33bbb0ab55feadf
770prince775 <bhardwajprince2635@gmail.com>
2025-11-12 11:23:38 +0530

    Add files via upload

---
+++ pol.java
@@ -0,0 +1,13 @@
+public class pol {
+    void hello(int a ){
+        System.out.println("i am say hello");
+    }
+    void hello(String b){
+        System.out.println(" i am say hy ");
+    }
+    public static void main(String[] args) {
+        pol p = new pol();
+        p.hello("mohan");  
+    } 
+    
+}
